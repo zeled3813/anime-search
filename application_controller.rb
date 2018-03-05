3428674f5e5a15622f4be81b5f03a0411e316a33
kathleens0782 <Kathleens0782@hstat.org>
2018-03-05 16:54:24 +0000

    added params

--- application_controller.rb
+++ application_controller.rb
@@ -11,7 +11,9 @@ class ApplicationController < Sinatra::Base
   end
   
   post '/result' do 
-    
+    @anime_name = params[:name]
+    @anime_description = params[:description]
+    @anime_date = params[:date]
     erb :result
   end
   
