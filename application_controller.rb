d3f1df48cc336af6ec89937399fee7fbc800a44e
zeled3813 <zeled3813@hstat.org>
2018-03-15 01:14:21 +0000

    finished with most of it

--- application_controller.rb
+++ application_controller.rb
@@ -11,10 +11,14 @@ class ApplicationController < Sinatra::Base
   end
   
   post '/result' do 
-    @anime_name = params[:name]
-    @anime_description = params[:description]
-    @anime_date = params[:date]
+    anime_name = params[:anime_name]
+    @anime_name_result = anime_name_search(anime_name)
+    @anime_desc_result = anime_description_search(anime_name)
+    @anime_episode_count_result = anime_episode_count_search(anime_name)
+    @anime_episode_length_result = anime_episode_length_search(anime_name)
+    @anime_date_release_result = anime_date_release_search(anime_name)
+    @anime_status_result = anime_status_search(anime_name)
+    @anime_image_url = anime_image_search(anime_name)
     erb :result
   end
-  
 end
--- models/kitsu.rb
+++ models/kitsu.rb
@@ -17,57 +17,59 @@ require 'pp'
 # anime_hash = JSON.parse(response)
 # puts anime_hash
 
-def anime_genre_search
-    url = 'https://kitsu.io/api/edge/anime?filter[genres]=horror'
-    # url = 'https://kitsu.io/api/edge/anime?page[limit]=1/filter[text]=cowboy%20bebop'
-    uri = URI(url)
-    response = Net::HTTP.get(uri)
-    anime_hash = JSON.parse(response)
-    result = " "
+# def anime_genre_search
+#     url = 'https://kitsu.io/api/edge/anime?filter[genres]=horror'
+#     # url = 'https://kitsu.io/api/edge/anime?page[limit]=1/filter[text]=cowboy%20bebop'
+#     uri = URI(url)
+#     response = Net::HTTP.get(uri)
+#     anime_hash = JSON.parse(response)
+#     result = " "
     
-    #Searching for specific anime title below.
-    anime_hash["data"].map do |key,value| 
-        key.each do |key_type,value_type|
-            if key_type == "attributes"
-                value_type.each do |category,detail|
-                    if category == "titles"
-                        detail.each do |language,title|
-                            if language == "en_jp"
-                                 puts title
-                            end
-                        end
-                    end
-                end
-            end
-        end
-    end
-    #Searching for specific anime title above.
+#     #Searching for specific anime title below.
+#     anime_hash["data"].map do |key,value| 
+#         key.each do |key_type,value_type|
+#             if key_type == "attributes"
+#                 value_type.each do |category,detail|
+#                     if category == "titles"
+#                         detail.each do |language,title|
+#                             if language == "en_jp"
+#                                  puts title
+#                             end
+#                         end
+#                     end
+#                 end
+#             end
+#         end
+#     end
+#     #Searching for specific anime title above.
     
-    # pp anime_hash
-    puts "The above are the titles in this genre"
-    puts "The following is the value of result:"
-    puts result
-end
+#     # pp anime_hash
+#     puts "The above are the titles in this genre"
+#     puts "The following is the value of result:"
+#     puts result
+# end
 
 # anime_genre_search
 
-def anime_name_search
-    url = 'https://kitsu.io/api/edge/anime?page[limit]=1/filter[text]=attack%20on%20titan'
+def anime_name_search(anime_name)
+    anime_name_hash = []
+    anime_name_hash << anime_name.downcase.split(" ")
+    anime_name_hash = anime_name_hash.join("%20")
+    url = 'https://kitsu.io/api/edge/anime?filter[text]=' + anime_name_hash
     # url = 'https://kitsu.io/api/edge/anime?filter[genres]=horror'
     uri = URI(url)
     response = Net::HTTP.get(uri)
     anime_hash = JSON.parse(response)
-    pp anime_hash["data"]
-    result = []
-    # puts anime_name
-    anime_hash["data"].map do |key,value| 
-        key.each do |key_type,value_type|
-            if key_type == "attributes"
-                value_type.each do |category,detail|
+    # pp anime_hash
+    name_result = ""
+    anime_hash["data"].map do |key|
+        key.each do |type_key,type_value|
+            if type_key == "attributes"
+                type_value.each do |category,detail|
                     if category == "titles"
                         detail.each do |language,title|
-                            if language == "en_jp"
-                                 puts "name: " + title
+                            if language == "en" && title.downcase == anime_name.downcase
+                                name_result = "Anime name: " + title.to_s
                             end
                         end
                     end
@@ -75,53 +77,234 @@ def anime_name_search
             end
         end
     end
-    def anime_date_search
-    url = 'https://kitsu.io/api/edge/anime?page[limit]=20'
-     uri = URI(url)
+    return name_result
+end
+
+# anime_name_search("Monster")
+
+def anime_description_search(anime_name)
+    anime_name_hash = []
+    anime_name_hash << anime_name.downcase.split(" ")
+    anime_name_hash = anime_name_hash.join("%20")
+    url = 'https://kitsu.io/api/edge/anime?filter[text]=' + anime_name_hash
+    uri = URI(url)
     response = Net::HTTP.get(uri)
     anime_hash = JSON.parse(response)
-    anime_hash["data"].each do |key,value|
-        key.each do |key_type,value_type|
-            if key_type == "attributes"
-                value_type.each do |title,language|
-                    puts language
-                end
-            end
-        end
-    end
-end
+    # pp anime_hash
+    desc_result = ""
     anime_hash["data"].map do |key|
         key.each do |type_key,type_value|
             if type_key == "attributes"
                 type_value.each do |category,detail|
-                    if category == "synopsis"
-                        puts "Description: " + detail.to_s
-                    end
-                    if category == "episodeCount"
-                        puts "Episode count: " + detail.to_s + " episodes"
-                    end
-                    if category == "episodeLength"
-                        puts "Episode length: " + detail.to_s + " min(s)"
+                    if category == "titles"
+                        detail.each do |language,title|
+                            if language == "en" && title.downcase == anime_name.downcase
+                                type_value.each do |category,detail|
+                                    if category == "synopsis"
+                                        desc_result = "Summary: " + detail.to_s
+                                    end
+                                end
+                                
+                            end
+                        end
                     end
                 end
             end
         end
     end
-
-    
-    puts "The above the return value of the iteration."
-    puts "The following is the result:"
-    puts result
-
-# anime_name_search
-
-url = 'https://kitsu.io/api/edge/anime?page[limit]=1/filter[text]=' + 'cowboy%20bebop'
-uri = URI(url)
-response = Net::HTTP.get(uri)
-pp JSON.parse(response)
-
-
-
+    return desc_result
 end
+
+# anime_description_search("Monster")
+
+def anime_episode_count_search(anime_name)
+    anime_name_hash = []
+    anime_name_hash << anime_name.downcase.split(" ")
+    anime_name_hash = anime_name_hash.join("%20")
+    url = 'https://kitsu.io/api/edge/anime?filter[text]=' + anime_name_hash
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    anime_hash = JSON.parse(response)
+    # pp anime_hash
+    episode_count_result = ""
+    anime_hash["data"].map do |key|
+        key.each do |type_key,type_value|
+            if type_key == "attributes"
+                type_value.each do |category,detail|
+                    if category == "titles"
+                        detail.each do |language,title|
+                            if language == "en" && title.downcase == anime_name.downcase
+                                type_value.each do |category,detail|
+                                    if category == "episodeCount"
+                                        episode_count_result = detail.to_s
+                                    end
+                                end
+                            end
+                        end
+                    end
+                end
+            end
+        end
+    end
+    return episode_count_result
+end
+
+# anime_episode_count_search("Monster")
+
+def anime_episode_length_search(anime_name)
+    anime_name_hash = []
+    anime_name_hash << anime_name.downcase.split(" ")
+    anime_name_hash = anime_name_hash.join("%20")
+    url = 'https://kitsu.io/api/edge/anime?filter[text]=' + anime_name_hash
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    anime_hash = JSON.parse(response)
+    # pp anime_hash
+    episode_length_result = ""
+    anime_hash["data"].map do |key|
+        key.each do |type_key,type_value|
+            if type_key == "attributes"
+                type_value.each do |category,detail|
+                    if category == "titles"
+                        detail.each do |language,title|
+                            if language == "en" && title.downcase == anime_name.downcase
+                                type_value.each do |category,detail|
+                                    if category == "episodeLength"
+                                        episode_length_result = detail.to_s + "min(s)"
+                                    end
+                                end
+                                
+                            end
+                        end
+                    end
+                end
+            end
+        end
+    end
+    return episode_length_result
+end
+
+# anime_episode_length_search("Monster")
+
+def anime_date_release_search(anime_name)
+    anime_name_hash = []
+    anime_name_hash << anime_name.downcase.split(" ")
+    anime_name_hash = anime_name_hash.join("%20")
+    url = 'https://kitsu.io/api/edge/anime?filter[text]=' + anime_name_hash
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    anime_hash = JSON.parse(response)
+    # pp anime_hash
+    anime_date_release_result = ""
+    anime_hash["data"].map do |key|
+        key.each do |type_key,type_value|
+            if type_key == "attributes"
+                type_value.each do |category,detail|
+                    if category == "titles"
+                        detail.each do |language,title|
+                            if language == "en" && title.downcase == anime_name.downcase
+                                type_value.each do |category,detail|
+                                    if category == "startDate"
+                                        anime_date_release_result = detail.to_s
+                                    end
+                                end
+                                
+                            end
+                        end
+                    end
+                end
+            end
+        end
+    end
+    return anime_date_release_result
+end
+
+# anime_date_release_search("Monster")
+
+def anime_status_search(anime_name)
+    anime_name_hash = []
+    anime_name_hash << anime_name.downcase.split(" ")
+    anime_name_hash = anime_name_hash.join("%20")
+    url = 'https://kitsu.io/api/edge/anime?filter[text]=' + anime_name_hash
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    anime_hash = JSON.parse(response)
+    pp anime_hash
+    anime_status_result = ""
+    anime_hash["data"].map do |key|
+        key.each do |type_key,type_value|
+            if type_key == "attributes"
+                type_value.each do |category,detail|
+                    if category == "titles"
+                        detail.each do |language,title|
+                            if language == "en" && title.downcase == anime_name.downcase
+                                type_value.each do |category,detail|
+                                    if category == "status"
+                                        anime_status_result = detail.to_s
+                                    end
+                                end
+                                
+                            end
+                        end
+                    end
+                end
+            end
+        end
+    end
+    return anime_status_result
+end
+
+anime_status_search("Monster")
+
+def anime_image_search(anime_name)
+    anime_name_hash = []
+    anime_name_hash << anime_name.downcase.split(" ")
+    anime_name_hash = anime_name_hash.join("%20")
+    url = 'https://kitsu.io/api/edge/anime?filter[text]=' + anime_name_hash
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    anime_hash = JSON.parse(response)
+    image_url = ""
+    anime_hash["data"].map do |key|
+        key.each do |type_key,type_value|
+            if type_key == "attributes"
+                type_value.each do |category,detail|
+                    if category == "titles"
+                        detail.each do |language,title|
+                            if language == "en" && title.downcase == anime_name.downcase
+                                type_value.each do |category,detail|
+                                    if category == "posterImage"
+                                        detail.each do |size,url|
+                                            if size == "original"
+                                                image_url = url
+                                            end
+                                        end
+                                    end
+                                end
+                                
+                            end
+                        end
+                    end
+                end
+            end
+        end
+        return image_url
+    end
+end
+
+# anime_image_search("Monster")
+
+
+
+# def testing(anime_name)
+#     anime_name_hash = []
+#     anime_name_hash << anime_name.downcase.split(" ")
+#     anime_name_hash = anime_name_hash.join("%20")
+#     puts anime_name_hash
+#     url = 'https://kitsu.io/api/edge/anime?filter[text]=' + anime_name_hash
+#     puts url
+# end
+
+# testing("Attack on titan")
 
 
